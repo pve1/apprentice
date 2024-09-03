872033f2bb2e1a6550fb4760325ab79ef1e10062
pve1 <pve1@foo.invalid>
2024-09-03 15:28:48 +0300

    Code.

---
+++ package.lisp
@@ -0,0 +1,11 @@
+
+(defpackage #:slime-apprentice
+  (:use #:cl)
+  (:export "*APPRENTICE*"
+           "*MAX-DESCRIPTION-SIZE*"
+           "*FORCE-RETURN-DESCRIPTION*"
+           "DESCRIPTION"
+           "PRESENTATION-DESCRIPTION"
+           "SYMBOL-DESCRIPTION"))
+
+(in-package :slime-apprentice)
