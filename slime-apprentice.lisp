be991d4d87fb5ff14515c98a54545725cb94d7bf
pve1 <pve1@foo.invalid>
2024-09-04 23:14:18 +0300

    describe-unknown-symbol

--- slime-apprentice.lisp
+++ slime-apprentice.lisp
@@ -1,4 +1,5 @@
-;;;; Requires 
+;;;; Requires
+;;;;   swank
 ;;;;   "package"
 
 (in-package :slime-apprentice) [*]
@@ -12,13 +13,21 @@
 (defvar *previous-object* nil)
 (defvar *previous-description* nil)
 
-(defgeneric Description (apprentice object stream)
+(defgeneric Describe-with-apprentice (apprentice object stream)
   (:method (apprentice object stream)
     (describe object stream)))
 
+(defgeneric Describe-unknown-symbol (apprentice symbol-name stream)
+  (:method (apprentice (symbol-name string) stream) ; read error, e.g "cl:mapca"
+    nil)
+  (:method (apprentice (symbol-name symbol) stream) ; uninterned symbol
+    nil))
+
+;; May return a string, :unchanged or :max-size-exceeded.
 (defun return-description (object desc)
+  (check-type desc string)
   (if (<= (length desc) *max-description-size*)
-      (prog1 (if (and (eql *previous-object* object)
+      (prog1 (if (and (equal *previous-object* object) ; may be a string if unknown symbol
                       (equal *previous-description* desc)
                       (not *force-return-description*))
                  :unchanged
@@ -29,11 +38,12 @@
 
 (defun Presentation-description (presentation-id)
   (let* ((desc (with-output-to-string (s)
-                (description *apprentice*
-                             (swank:lookup-presented-object
-                              presentation-id)
-                             s))))
-    (return-description presentation-id desc)))
+                 (describe-with-apprentice *apprentice*
+                                           (swank:lookup-presented-object
+                                            presentation-id)
+                                           s))))
+    (when desc
+      (return-description presentation-id desc))))
 
 ;; Read symbol-name in an empty, special package and try to figure out
 ;; which symbol was actually meant.
@@ -41,11 +51,15 @@
   (check-type symbol-name string)
   (let* ((preliminary-symbol
            (ignore-errors
-            (let ((*package* (find-package "SLIME-APPRENTICE-READ"))
-                  (*read-eval* nil))
-              (read-from-string symbol-name))))
+            (let* ((*package* (find-package "SLIME-APPRENTICE-READ"))
+                   (*read-eval* nil)
+                   (result (read-from-string symbol-name)))
+              (when (and result (symbolp result))
+                result))))
          (actual-symbol
            ;; Either foo:bar, :foo or  #:foo was read.
+           ;; TODO: Check for a keyword before reading, so we don't
+           ;; intern unnecessarily.
            (cond ((and preliminary-symbol
                        (symbolp preliminary-symbol)
                        (not (eql (symbol-package preliminary-symbol)
@@ -59,8 +73,24 @@
     (when (symbolp preliminary-symbol)
       (unintern preliminary-symbol (find-package "SLIME-APPRENTICE-READ")))
     (if actual-symbol
-        (let ((desc (with-output-to-string (s)
-                      (description *apprentice* actual-symbol s))))
-          (return-description actual-symbol desc))
-        nil)))
+        (return-description
+         actual-symbol
+         (with-output-to-string (s)
+           (describe-with-apprentice *apprentice*
+                                     actual-symbol
+                                     s)))
+        (if (and preliminary-symbol ; #:foo
+                 (symbolp preliminary-symbol))
+            (return-description
+             preliminary-symbol
+             (with-output-to-string (s)
+               (describe-unknown-symbol *apprentice*
+                                        preliminary-symbol
+                                        s)))
 
+            (return-description
+             symbol-name
+             (with-output-to-string (s)
+               (describe-unknown-symbol *apprentice*
+                                        symbol-name
+                                        s)))))))
