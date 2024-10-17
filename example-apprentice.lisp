cb72f951e641281e40a952c2f4d25d9847cba42e
pve1 <pve1@foo.invalid>
2024-10-18 00:29:06 +0300

    Example-apprentice formatting

--- example-apprentice.lisp
+++ example-apprentice.lisp
@@ -12,7 +12,7 @@
 ;;;;   "suggest-apprentice"
 ;;;;   "buttons"
 
-(in-package :apprentice) 
+(in-package :apprentice)
 
 (defclass example-apprentice (apprentice-gathering)
   ((toplevel :initarg :toplevel
@@ -37,14 +37,14 @@
       (format t "~A names a package: " object)
       (put-lisp-button-here *apprentice*
                             "[DEL]"
-                            `(delete-package 
+                            `(delete-package
                               (find-package ',object))
                             :redisplay t)
       (princ " ")
       (put-lisp-button-here *apprentice*
                             "[CLREXT]"
                             `(loop :with pkg = (find-package ',object)
-                                   :for sym :being 
+                                   :for sym :being
                                    :each :external-symbol
                                    :in pkg
                                    :do (unexport sym pkg))
@@ -53,7 +53,7 @@
       (put-lisp-button-here *apprentice*
                             "[CLRSYM]"
                             `(loop :with pkg = (find-package ',object)
-                                   :for sym :being 
+                                   :for sym :being
                                    :each :symbol
                                    :in pkg
                                    :do (unintern sym pkg))
@@ -67,18 +67,18 @@
                           :external-symbol :in package
                           :collect sym)
                     #'string<))
-      t))) 
+      t)))
 
 (defun example-method-description (method)
   (format nil "~S (~{~S~^ ~})"
-          (closer-mop:generic-function-name 
+          (closer-mop:generic-function-name
            (closer-mop:method-generic-function method))
           (mapcar (lambda (spec)
                     (if (typep spec 'class)
                         (class-name spec)
                         (list 'eql
                               (closer-mop:eql-specializer-object spec))))
-                  (closer-mop:method-specializers 
+                  (closer-mop:method-specializers
                    method))))
 
 (defun example-describe-class-maybe (object stream)
@@ -89,7 +89,7 @@
            (class (find-class object))
            (methods (closer-mop:specializer-direct-methods
                      class))
-           (descriptions 
+           (descriptions
              (sort (mapcar 'example-method-description methods)
                    #'string<)))
       (when methods
@@ -97,7 +97,7 @@
         (format t "~{~A~^~%~}" descriptions)
         t))))
 
-(defmethod describe-with-apprentice ((ap example-apprentice) 
+(defmethod describe-with-apprentice ((ap example-apprentice)
                                      (c looking-at-character)
                                      stream)
   (setf (state ap) 'toplevel)
@@ -105,25 +105,24 @@
     (setf (state ap) nil)))
 
 (let ((*caching-apprentice-default-update-interval* 1))
-  (setf *apprentice* 
-        (make-instance 'example-apprentice 
+  (setf *apprentice*
+        (make-instance 'example-apprentice
           :toplevel (list (make-instance 'suggest-apprentice)
                           (make-instance 'wide-toplevel-apprentice))
-          :apprentices 
-          (list  
+          :apprentices
+          (list
            'suggest-apprentice
            'describe-apprentice
            #'example-describe-package-maybe
            'value-apprentice
            'grep-apprentice
-           ;; 'history-apprentice
            (make-instance 'apropos-apprentice
              :min-length 2
              :interesting-symbol-function
              (lambda (sym)
                ;; Ignore mixed case symbols.
                (if (eq (symbol-package sym) *package*)
-                   (not (find-if #'lower-case-p (symbol-name sym)))                   
+                   (not (find-if #'lower-case-p (symbol-name sym)))
                    t)))
            #'example-describe-class-maybe))))
 
