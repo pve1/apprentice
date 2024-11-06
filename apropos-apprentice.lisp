3eec1a9a721917f68f4aec08f741ef8246fe7d5a
pve1 <pve1@foo.invalid>
2024-11-06 11:45:26 +0200

    Improve apropos-apprentice

--- apropos-apprentice.lisp
+++ apropos-apprentice.lisp
@@ -63,66 +63,118 @@
         :when (search string (package-name p))
         :collect p))
 
+(defmethod apropos-compute-interesting-symbols ((ap apropos-apprentice)
+                                                (input-symbol symbol))
+  (let* ((package-indicator-2 (getf *buffer-context*
+                                    'package-indicator))
+         (other-symbols (make-hash-table))
+         (inherited-symbols (make-hash-table))
+         (present-symbols (make-hash-table))
+         (interesting-packages)
+         (interesting-symbol-count 0))
+    (cond ((eq package-indicator-2 :current) ; no package prefix
+           (push *package* interesting-packages))
+          ((eq package-indicator-2 :keyword)
+           (push (find-package :keyword) interesting-packages))
+          (t (dolist (pkg (packages-with-names-like
+                           package-indicator-2))
+               (pushnew pkg interesting-packages))))
+    (let ((interesting-symbol-function
+            (interesting-symbol-function ap)))
+      (dolist (pkg interesting-packages)
+        (dolist (sym (apropos-list input-symbol pkg))
+          (when (funcall interesting-symbol-function sym)
+            (multiple-value-bind (symbol status)
+                (find-symbol (symbol-name sym) pkg)
+              (when (or (eq pkg *package*)
+                        (eq status :external))
+              (incf interesting-symbol-count)
+              (cond ((and (eql pkg *package*)
+                          (member status '(:external :internal)))
+                     (setf (gethash sym present-symbols) t))
+                    ((eql pkg *package*)
+                     (setf (gethash sym inherited-symbols) t))
+                    (t (setf (gethash sym other-symbols) t))))))))
+    (unless (zerop interesting-symbol-count)
+      (list :present (alexandria:hash-table-keys present-symbols)
+            :inherited (alexandria:hash-table-keys inherited-symbols)
+            :other (alexandria:hash-table-keys other-symbols))))))
+
+(defmethod apropos-export-symbols ((ap apropos-apprentice)
+                                   root-symbol
+                                   &key breadth
+                                        (package *package*))
+  (assert (member breadth '(nil :present :all)))
+  (destructuring-bind (&key present
+                            inherited
+                            other)
+      (apropos-compute-interesting-symbols ap root-symbol)
+    (ecase breadth
+      ((:present nil)
+       (export present package)
+       present))))
+
+(defmethod apropos-export-button ((ap apropos-apprentice) symbol
+                                  &key offset)
+  (put-lisp-button-here
+   ap "[EXPORT]"
+   `(progn
+      (let ((symbols (apropos-export-symbols *button-apprentice*
+                                             ',symbol)))
+        (emacs-message
+         (format nil "Exported ~A symbols from ~S."
+                 (length symbols)
+                 (package-name *package*)))))
+   :offset offset))
+
+(defmethod apropos-import-symbols ((ap apropos-apprentice)
+                                   root-symbol
+                                   &key (package *package*))
+  (destructuring-bind (&key present
+                            inherited
+                            other)
+      (apropos-compute-interesting-symbols ap root-symbol)
+    (when other
+      (import other package)
+      other)))
+
+(defmethod apropos-import-button ((ap apropos-apprentice) symbol
+                                  &key offset)
+  (put-lisp-button-here
+   ap "[IMPORT]"
+   `(progn
+      (let ((symbols (apropos-import-symbols *button-apprentice*
+                                             ',symbol)))
+        (emacs-message
+         (format nil "Imported ~A symbols into ~S."
+                 (length symbols)
+                 (package-name *package*)))))
+   :offset offset))
+
+;; TODO: add import button for "other"
 (defmethod apprentice-update ((ap apropos-apprentice)
                               (object symbol))
   (alexandria:when-let* ((fn (ignore-input-function ap))
                          (ignore (funcall fn object)))
     (return-from apprentice-update nil))
-  (let* ((package-indicator (get object 'package-indicator))
-         (symbol-package (symbol-package object))
-         (symbols (make-hash-table))
-         (inherited-symbols (make-hash-table))
-         (non-inherited-symbols (make-hash-table))
-         (interesting-packages)
-         (interesting-symbol-count 0))
-    (with-output-to-string (*standard-output*)
-      (when package-indicator
-        (dolist (pkg (packages-with-names-like package-indicator))
-          (pushnew pkg interesting-packages)))
-      (when symbol-package
-        ;; In cases like "some-values" doing apropos on "values" will
-        ;; only show symbols from CL, not MY-PACKAGE::SOME-VALUES. The
-        ;; following form fixes that.
-        (when (member symbol-package (package-use-list *package*))
-          (push *package* interesting-packages))
-        (dolist (pkg (packages-with-names-like
-                      (package-name symbol-package)))
-          (pushnew pkg interesting-packages)))
-      (let ((interesting-symbol-function
-              (interesting-symbol-function ap)))
-        (dolist (pkg interesting-packages)
-          (dolist (sym (apropos-list object pkg (not (eq pkg *package*))))
-            (unless (eq sym object)
-              (when (funcall interesting-symbol-function sym)
-                (incf interesting-symbol-count)
-                (cond ((eql *package* (symbol-package sym))
-                       (setf (gethash sym non-inherited-symbols) t))
-                      ((eql pkg *package*)
-                       (setf (gethash sym inherited-symbols) t))
-                      (t (setf (gethash sym symbols) t))))))))
-      (when (zerop interesting-symbol-count)
-        (return-from apprentice-update nil))
-      (fresh-line)
-      (princ "Apropos: ")
-      (terpri)
-      (unless (zerop (hash-table-count non-inherited-symbols))
-        (loop :with did-print
-              :for key :being :each :hash-key
-              :in non-inherited-symbols
-              :do (print key)
-                  (setf did-print t)
-              :finally (when did-print
-                         (terpri))))
-      (unless (zerop (hash-table-count inherited-symbols))
-        (loop :with did-print
-              :for key :being :each :hash-key
-              :in inherited-symbols
-              :do (print key)
-                  (setf did-print t)
-              :finally (when did-print
-                         (terpri))))
-      (unless (zerop (hash-table-count symbols))
-        (loop :for key :being :each :hash-key
-              :in symbols
-              :unless (gethash key inherited-symbols)
-              :do (print key))))))
+  (let* ((interesting-symbols
+           (apropos-compute-interesting-symbols ap object))
+         (offset (file-position *standard-output*)))
+    (when interesting-symbols
+      (with-output-to-string (*standard-output*)
+        (princ "Apropos: ")
+        (when (getf interesting-symbols :present)
+          (apropos-export-button ap object :offset offset))
+        (when (getf interesting-symbols :other)
+          (princ " ")
+          (apropos-import-button ap object :offset offset))
+        (terpri)
+        (destructuring-bind (&key inherited present other)
+            interesting-symbols
+          (let ((*package* (find-package :keyword)))
+            (dolist (symbol present)
+              (print symbol))
+            (dolist (symbol inherited)
+              (print symbol))
+            (dolist (symbol other)
+              (print symbol))))))))
