a0287d6b9922a382118438b9571f8572e75d0d64
pve1 <pve1@foo.invalid>
2024-11-13 00:09:23 +0200

    Renamed parameter

--- symbols.lisp
+++ symbols.lisp
@@ -2,26 +2,25 @@
 
 (in-package :apprentice) cx
 
-(defun symbol-status (symbol &optional (package *package*))
-  (nth-value 1 (find-symbol (string symbol) package)))
+(defun symbol-status (symbol-designator &optional (package *package*))
+  (nth-value 1 (find-symbol (string symbol-designator) package)))
 
 (defun symbol-home-package-p (symbol &optional (package *package*))
   (eq package (symbol-package symbol)))
 
-(defun symbol-present-p (symbol &optional (package *package*))
-  (let ((status (symbol-status symbol package)))
+(defun symbol-present-p (symbol-designator &optional (package *package*))
+  (let ((status (symbol-status symbol-designator package)))
     (or (eq status :internal)
         (eq status :external))))
 
-(defun symbol-internal-p (symbol &optional (package *package*))
-  (let ((status (symbol-status symbol package)))
+(defun symbol-internal-p (symbol-designator &optional (package *package*))
+  (let ((status (symbol-status symbol-designator package)))
     (eq status :internal)))
 
-(defun symbol-external-p (symbol &optional (package *package*))
-  (let ((status (symbol-status symbol package)))
+(defun symbol-external-p (symbol-designator &optional (package *package*))
+  (let ((status (symbol-status symbol-designator package)))
     (eq status :external)))
 
-(defun symbol-inherited-p (symbol &optional (package *package*))
-  (let ((status (symbol-status symbol package)))
+(defun symbol-inherited-p (symbol-designator &optional (package *package*))
+  (let ((status (symbol-status symbol-designator package)))
     (eq status :inherited)))
-
