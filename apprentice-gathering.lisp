af2e69eab760dcf5557407cd4116c9ac0c9a4256
pve1 <pve1@foo.invalid>
2024-11-14 13:13:16 +0200

    Improved apprentice-gathering

--- apprentice-gathering.lisp
+++ apprentice-gathering.lisp
@@ -12,6 +12,9 @@
 (defmethod Apprentice-gathering-divider (apprentice)
   "-------------------------------------------------------------")
 
+(defmethod Apprentices-based-on-input (apprentice input)
+  (apprentices apprentice))
+
 (defmethod describe-with-apprentice ((ap apprentice-gathering)
                                      object
                                      stream)
@@ -23,7 +26,7 @@
                  (princ (apprentice-gathering-divider ap) stream))
                (fresh-line stream)
                (setf divider-printed t))))
-      (dolist (a (apprentices ap))
+      (dolist (a (apprentices-based-on-input ap object))
         (print-divider-maybe)
         (when (describe-with-apprentice a object stream)
           (fresh-line stream)
@@ -32,22 +35,24 @@
       ;; regardless of what the last apprentice printed.
       (print-divider-maybe))))
 
-(defmethod initialize-instance :after ((a apprentice-gathering) &key apprentices)
-  (setf (apprentices a) (mapcar (lambda (x)
-                                  (if (symbolp x)
-                                      (make-instance x)
-                                      x))
-                                apprentices)))
+(defun instantiate-maybe (thing)
+  (cond ((null thing)
+         thing)
+        ((keywordp thing)
+         thing)
+        ((symbolp thing)
+         (make-instance thing))
+        (t thing)))
+
+(defmethod initialize-instance :after ((a apprentice-gathering)
+                                       &key apprentices)
+  (setf (apprentices a) (mapcar 'instantiate-maybe apprentices)))
 
 (defun apprentice-gathering (&rest apprentices)
   (make-instance 'apprentice-gathering
-    :apprentices (mapcar (lambda (x)
-                           (if (symbolp x)
-                               (make-instance x)
-                               x))
-                         apprentices)))
+    :apprentices (mapcar 'instantiate-maybe apprentices)))
 
-(defun Make-example-apprentice-gathering ()
+(defun make-example-apprentice-gathering ()
   (let ((*caching-apprentice-default-update-interval* 2))
     (apprentice-gathering 'describe-apprentice
                           'value-apprentice
