ca23b9f3c39cf070c38a03c7f5d8998a320aca66
pve1 <pve1@foo.invalid>
2025-05-12 11:59:00 +0300

    Added export-capitalized-from

--- package.lisp
+++ package.lisp
@@ -10,3 +10,13 @@
 (define-symbol-macro cx
     (eval-when (:compile-toplevel :load-toplevel :execute)
       (setf *readtable* (capitalized-export:make-capitalized-export-readtable))))
+
+(defmacro export-capitalized-from (package-name)
+  `(eval-when (:compile-toplevel :load-toplevel :execute)
+     (unless (find-package ',package-name)
+       (make-package ',package-name :use '()))
+     (let ((scanner (make-instance 'capitalized-export:default-scanner
+                      :export-from-package ',package-name
+                      :read-into-package *package*)))
+       (setf *readtable* (capitalized-export:make-capitalized-export-readtable
+                          :scanner scanner)))))
